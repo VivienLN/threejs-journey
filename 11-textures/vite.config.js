7ad92f1618cb55c21c6cdbd49af07d89c8375f21
VivienLN <vivienleneez@gmail.com>
2023-01-23 15:12:52 +0100

    update to vite (+ changed server.open to false for docker setup)

---
+++ 11-textures/vite.config.js
@@ -0,0 +1,20 @@
+const isCodeSandbox = !!process.env.SANDBOX_URL
+
+export default {
+    root: "src/",
+    publicDir: "../static/",
+    base: "./",
+    server:
+    {
+        host: true,
+        // For docker: set to false
+        open: false
+        // open: !isCodeSandbox // Open if it's not a CodeSandbox
+    },
+    build:
+    {
+        outDir: "../dist",
+        emptyOutDir: true,
+        sourcemap: true
+    }
+}
