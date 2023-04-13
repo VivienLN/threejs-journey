e6c5ea63f13be75adc334343ddaa62c805c19587
VivienLN <vivienleneez@gmail.com>
2023-04-13 10:59:19 +0200

    varyings

--- 27-shaders/src/shaders/test/fragment.glsl
+++ 27-shaders/src/shaders/test/fragment.glsl
@@ -1,6 +1,12 @@
 precision mediump float;
 
+varying float vRandom;
+
 void main()
 {
-    gl_FragColor = vec4(1.0, 0.5, 0.0, 1.0);
+    // Use vRandom
+    // gl_FragColor = vec4(vRandom, 1.0-vRandom, 0.6, 1.0);
+
+    // Same color for all fragments
+    gl_FragColor = vec4(1.0, 0.6, 1.0, 1.0);
 }
--- 27-shaders/src/shaders/test/vertex.glsl
+++ 27-shaders/src/shaders/test/vertex.glsl
@@ -3,8 +3,27 @@ uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
 
 attribute vec3 position;
+attribute float aRandom;
+
+varying float vRandom;
+
+#define M_PI 3.1415926535897932384626433832795
 
 void main()
 {
-    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+    float waves = 4.0;
+    float maxZ = .05;
+
+    // All in one line
+    // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+    // Separated 
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    modelPosition.z += sin(modelPosition.x * M_PI * waves) * maxZ;
+    modelPosition.z += aRandom * .06;
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+
+    gl_Position = projectedPosition;
+
+    vRandom = aRandom;
 }
